1f45182fd58cd783475160baa6a4a01efbf6429c
xytus <xytusjoseph123@gmail.com>
2024-10-04 02:48:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """
-    <h1>Welcome to Our Simple Flask Web Application</h1>
-    <p>This is a basic Flask web application used for demonstrating CI/CD and security testing.</p>
-    <p>Created by: Col Moschin</p>
-    """
+    return "<h1>Welcome to the Simple Flask App</h1><p>This application was created by Col Moschin.</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
